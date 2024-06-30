5425dbcb97c38c3c5bea0f25c24ea24e2b7fbb6c
NotWhiteRice <42843154+NotWhiteRice@users.noreply.github.com>
2024-06-30 11:58:46 -0400

    Update EndlessSkies.java

--- src/main/java/io/github/notwhiterice/endlessskies/EndlessSkies.java
+++ src/main/java/io/github/notwhiterice/endlessskies/EndlessSkies.java
@@ -7,8 +7,22 @@ public class EndlessSkies {
     public static final String modID = "endlessskies";
 
     public EndlessSkies() {
-        IEventBus bus;
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        //CreativeModeTabInit.register(bus);
+
+        //ItemInit.register(bus);
         BlockInit.register(bus);
+
+        bus.addListener(this::onClientSetup);
+        bus.addListener(this::onBuildCreativeModeTabContents);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event) {
+        
+    }
+
+    private void onBuildCreativeModeTabContents(BuildCreativeModeTabContentsEvent event) {
+        
     }
 }
