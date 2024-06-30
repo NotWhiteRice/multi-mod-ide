cea3408479b3f3be2d1d330e44d0dbcfec26ffed
NotWhiteRice <42843154+NotWhiteRice@users.noreply.github.com>
2024-06-30 11:59:48 -0400

    Update EndlessSkies.java

--- src/main/java/io/github/notwhiterice/endlessskies/EndlessSkies.java
+++ src/main/java/io/github/notwhiterice/endlessskies/EndlessSkies.java
@@ -1,6 +1,16 @@
 package io.github.notwhiterice.endlessskies;
 
+import io.github.notwhiterice.endlessskies.init.BlockInit;
+//import io.github.notwhiterice.endlessskies.init.CreativeModeTabInit;
+//import io.github.notwhiterice.endlessskies.init.ItemInit;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(EndlessSkies.modID)
 public class EndlessSkies {
